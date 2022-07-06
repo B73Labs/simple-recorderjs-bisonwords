3f890d56d01e0e7dab9aeb0738c8bb464cbc6e51
Michael Best <mbest@arcticlinux.com>
2022-07-06 10:37:56 -0600

    Fix bug in new upload.php

--- bisonwords/upload.php
+++ bisonwords/upload.php
@@ -5,12 +5,10 @@ $size = $_FILES['audio_data']['size']; //the size in bytes
 $tmpName = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
 $name = $_FILES['audio_data']['name'];
 foreach ($words as $word) {
-    if ($name == $word['slug']) {
-        $basename = $slug;
-        $outputName = uniqid($basename . '-', true) . '.wav';
+	$slug = $word['slug'];
+    if ($name == $slug) {
+        $outputName = uniqid($slug . '-', true) . '.wav';
         //move the file from temp name to local folder using $output name
         move_uploaded_file($tmpName, 'recordings/' . $outputName);
     }
 }
-
-
